43b59dab1683e19380811e64ec2149359fba12a6
Winston Jade Molit <winston.molit@zynappse.com>
2024-09-07 21:51:11 +0800

    Reduce Initial JavaScript Execution Time

--- js/products.js
+++ js/products.js
@@ -65,12 +65,21 @@ function displayProducts(products) {
 
 }
 
+window.onload = () => {
+    let status = 'idle';
+    let productSection = document.querySelector("#all-products");
 
+    window.onscroll = () => {
+        let position = productSection.getBoundingClientRect().top - (window.scrollY + window.innerHeight);
+        if (status == 'idle' && position <= 0) {
+            status = 'fetching';
+            loadProducts();
 
-loadProducts();
-
-// Simulate heavy operation. It could be a complex price calculation.
-for (let i = 0; i < 10000000; i++) {
-    const temp = Math.sqrt(i) * Math.sqrt(i);
+            // Simulate heavy operation. It could be a complex price calculation.
+            for (let i = 0; i < 10000000; i++) {
+                const temp = Math.sqrt(i) * Math.sqrt(i);
+            }
+        }
+    }
 }
 
